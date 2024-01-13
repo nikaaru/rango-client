31aebc28f6b0ff8718b8774565ec18642a3a208f
Nikaru <nikaru@rango.exchange>
2024-01-13 17:03:52 +0000

    chore: test close pr

--- widget/embedded/src/pages/SettingsPage.tsx
+++ widget/embedded/src/pages/SettingsPage.tsx
@@ -181,7 +181,7 @@ export function SettingsPage() {
   return (
     <Layout
       header={{
-        title: i18n.t('Settings'),
+        title: i18n.t('Settings test close pr'),
       }}>
       <SettingsContainer>
         <Slippage />
