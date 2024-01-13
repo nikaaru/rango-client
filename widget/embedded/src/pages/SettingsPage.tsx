7656937f8cdfff0325fdf8f587d6f79998bbb3d3
Nikaru <nikaru@rango.exchange>
2024-01-13 16:22:09 +0000

    chore: fix setting page title

--- widget/embedded/src/pages/SettingsPage.tsx
+++ widget/embedded/src/pages/SettingsPage.tsx
@@ -181,7 +181,7 @@ export function SettingsPage() {
   return (
     <Layout
       header={{
-        title: i18n.t('Settings feat'),
+        title: i18n.t('Settings'),
       }}>
       <SettingsContainer>
         <Slippage />
