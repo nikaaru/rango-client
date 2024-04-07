178140c29c212042c4e86da30828901d5cccfe57
Nikaru <nikaru@rango.exchange>
2024-04-07 12:33:50 +0000

    feat: add preview urll to check workflow

--- scripts/deploy/command.mjs
+++ scripts/deploy/command.mjs
@@ -4,16 +4,44 @@ import process from 'node:process';
 import { workspacePackages } from '../common/utils.mjs';
 import { build } from '../publish/build.mjs';
 import { logAsSection } from '../publish/utils.mjs';
-import { deployProjectsToVercel } from './utils.mjs';
+import { deployProjectsToVercel, getVercelProjectId } from './utils.mjs';
+import { ENABLE_PREVIEW_DEPLOY } from './config.mjs';
 
 // TODO: Working directory should be empty.
 async function run() {
+
+  /*
+    Deploys packages based on the state of the `ENABLE_PREVIEW_DEPLOY` environment variable.
+    if ENABLE_PREVIEW_DEPLOY is true, only packages that has project id in workflow environments will be deployed.
+    else private packages will be deployed.
+ */
+
   // Detect last relase and what packages has changed since then.
   const packages = await workspacePackages();
+
   const privatePackages = packages.filter((pkg) => {
-    return pkg.private;
+    if (ENABLE_PREVIEW_DEPLOY) {
+      const hasProjectId = getVercelProjectId(pkg.name) && getVercelProjectId(pkg.name) !== 'NOT SET' ;
+      return pkg.private && hasProjectId;
+    } 
+    else {
+      return pkg.private;
+    }
   });
 
+
+  if(ENABLE_PREVIEW_DEPLOY){
+    console.log('preview deployment is enabled.');
+    console.log('these packages will be deployed:', privatePackages.map(pkg=>pkg.name).join(', '));
+    console.log('note: if you need add more packages to be deployed, first you need to add vercel project id to workflow environments then follow documentation there.');
+  }
+  else{
+    console.log('preview deployment is disabled.');
+    console.log('these packages will be deployed:', privatePackages.map(pkg=>pkg.name).join(', '));
+  }
+  logAsSection('[x] Check Environment');
+
+  
   await build(privatePackages).catch((e) => {
     console.log(
       '[-] BUILD FAILED. Ignore it to workflow run the rest of tasks.'
