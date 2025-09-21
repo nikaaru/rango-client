8856590fcbf24333384c767c1e9296e2531feea8
Nikaru <nikaru@rango.exchange>
2025-09-21 01:15:46 +0000

    feat: update title of history page

--- widget/embedded/src/pages/HistoryPage.tsx
+++ widget/embedded/src/pages/HistoryPage.tsx
@@ -171,7 +171,7 @@ export function HistoryPage() {
   return (
     <Layout
       header={{
-        title: i18n.t('History'),
+        title: i18n.t('History version4'),
         suffix: (
           <SuffixContainer>
             <Button
